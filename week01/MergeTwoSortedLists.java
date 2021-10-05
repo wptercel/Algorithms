388ac80592bec70f06103519c75c904ad6df871a
Peng Wu <wptercel@users.noreply.github.com>
2021-10-05 00:24:07 -0700

    week01-merge two sorted lists

---
+++ week01/MergeTwoSortedLists.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode p1 = l1;
+        ListNode p2 = l2;
+        ListNode preHead = new ListNode();
+        ListNode p = preHead;
+        while(p1 != null && p2 != null) {
+            if(p1.val <= p2.val) {
+                p.next = p1;
+                p1 = p1.next;
+            } else {
+                p.next = p2;
+                p2 = p2.next;
+            }
+            p = p.next;
+        }
+        p.next = p1 != null ? p1 : p2;
+        
+        return preHead.next;
+    }
+}
